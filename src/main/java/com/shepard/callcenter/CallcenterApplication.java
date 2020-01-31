a6bba6e2e6264960cb0f0661db88c4bea92ffccd
Alex Misha <alekseev.shepard@gmail.com>
2020-01-31 15:43:11 +0300

    init code commit

---
+++ src/main/java/com/shepard/callcenter/CallcenterApplication.java
@@ -0,0 +1,13 @@
+package com.shepard.callcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallcenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallcenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shepard/callcenter/CallcenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shepard.callcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallcenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
