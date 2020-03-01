83306db706df1b4d32d9354da25f95bba3f19ae4
Alex Misha <alekseev.shepard@gmail.com>
2020-03-01 18:46:05 +0300

    Download files controller

---
+++ backend/src/main/java/com/shepard/callcenter/config/WebConfig.java
@@ -0,0 +1,12 @@
+package com.shepard.callcenter.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Alex Misha
+ */
+@Configuration
+@ComponentScan(basePackages = "com.shepard.callcenter.controller")
+public class WebConfig {
+}
